a2762ed537c3c2eb917aa5992fc9b251fd5996d8
Emily <emilybribragg@gmail.com>
2021-12-01 13:02:02 -0500

    commit tree aa7be921a3c5f6b777497ada903be551e12c85e1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+function findMatching(drivers){
+    if (drivers === 'Bobby')
+    //takes an array of drivers and a string
+    return drivers// returns matching list of drivers
+}
