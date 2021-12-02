b4df63f3b923ce4b59d228042049eb88e19948c1
Emily <emilybribragg@gmail.com>
2021-12-01 20:20:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivers, name) {
+const listOfDrivers = drivers.filter(string => string.toLowerCase() === name.toLowerCase()) // the () is the callback function
+return listOfDrivers
+}
+
+
+function fuzzyMatch(drivers, letters) {
+const listOfLetters = drivers.filter(string => string.startsWith(letters))
+return listOfLetters
+}
+
+function matchName(drivers, string){
+const newName = drivers.filter(object => object.name === string)
+return newName
+}
+
+/*
+// original code
+function findMatching(drivers){
+    if (drivers === 'Bobby')
+    return drivers
+}
+
+// need a callback function?
+function findMatching(drivers, name) {
+
+}
+*/
